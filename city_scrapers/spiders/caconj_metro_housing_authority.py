a810be705d69f37a458b32577d0ea36f5c845993
Cruz Núñez <cruz@Cruzs-15-MacBook-Pro.local>
2025-01-03 14:38:26 -0500

    add Cincinnati Metro Housing Authority spider

---
+++ city_scrapers/spiders/caconj_metro_housing_authority.py
@@ -0,0 +1,98 @@
+import scrapy
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
+import re
+
+
+class CaconjMetroHousingAuthoritySpider(CityScrapersSpider):
+    name = "caconj_metro_housing_authority"
+    agency = "Cincinnati Metro Housing Authority"
+    timezone = "America/Chicago"
+    # start_urls = ["https://cintimha.com/wp-admin/admin-ajax.php?action=get_calendar_events&noheader=true&start_date=1719938796&end_date=1751475199&show_expired=true&event_category_id=executive-office-1466450825"]
+
+    def start_requests(self):
+        # generate start and endtime for api request
+        current_date = datetime.now()
+        start_date = current_date - relativedelta(months=6)
+        end_date = current_date + relativedelta(months=6)
+        start_int = start_date.strftime('%s')
+        end_int = end_date.strftime('%s')
+        # generate url with stand & end times
+        url = f"https://cintimha.com/wp-admin/admin-ajax.php?action=get_calendar_events&noheader=true&start_date={start_int}&end_date={end_int}&show_expired=true&event_category_id=executive-office-1466450825"
+
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        """
+        Parse API response from Executive Office cintimha.com calendar.
+        """
+
+        data = response.json()
+
+        for item in data:
+            meeting = Meeting(
+                title=item["title"],
+                description="",
+                classification=COMMISSION,
+                start=self._parse_start(item),
+                end=self._parse_end(item),
+                all_day=item["allDay"],
+                time_notes="",
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        start = item["start"]
+        start_no_tz = parse(start, ignoretz=True)
+        return start_no_tz
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        end = item["end"]
+        end_no_tz = parse(end, ignoretz=True)
+        return end_no_tz
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        location = {"name": "", "address": ""}
+        text = item["description"]
+        regex = r'(at)?.+\s(at|the)\s?(.+)\sbeginning'
+        match = re.search(regex, text)
+        if match:
+            address = match.group(3)
+            location["address"] = address
+            if "1635 Western Avenue" in address:
+                # set location name to boardroom if certain address found
+                location["name"] = "CMHA Boardroom"
+            else:
+                # split string up between name and address
+                # ex: West Union Square, 2942 Banning Road, Cincinnati, OH 45239
+                # name: West Union Square
+                # address: 2942 Banning Road, Cincinnati, OH 45239
+                parts = address.split(', ')
+                location["name"] = parts[0]
+                location["address"] = ', '.join(parts[1:])
+        return location
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        return [{
+            "title": "Event Link",
+            "href": item["url"]
+        }]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
---
+++ tests/test_caconj_metro_housing_authority.py
@@ -0,0 +1,117 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMISSION, CANCELLED, PASSED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.caconj_metro_housing_authority import CaconjMetroHousingAuthoritySpider
+import textwrap
+
+test_response = file_response(
+    join(dirname(__file__), "files", "caconj_metro_housing_authority.html"),
+    url="https://cintimha.com/wp-admin/admin-ajax.php?action=get_calendar_events&noheader=true&start_date=1719938796&end_date=1751475199&show_expired=true&event_category_id=executive-office-1466450825",
+)
+spider = CaconjMetroHousingAuthoritySpider()
+
+freezer = freeze_time("2025-01-02")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+def test_count():
+    assert len(parsed_items) == 6
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board of Commissioners Meeting - RESCHEDULED"
+    assert parsed_items[1]["title"] == "Board of Commissioners meeting - Rescheduled from August 20"
+    assert parsed_items[2]["title"] == "Board of Commissioners Meeting"
+    assert parsed_items[5]["title"] == "Annual & Board of Commissioners Meetings"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2024, 8, 20, 18, 0)
+    assert parsed_items[5]["start"] == datetime(2024, 12, 17, 17, 45)
+
+
+def test_end():
+    assert parsed_items[0]["end"] == datetime(2024, 8, 20, 20, 0)
+    assert parsed_items[5]["end"] == datetime(2024, 12, 17, 20, 0)
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "caconj_metro_housing_authority/202408201800/x/board_of_commissioners_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == CANCELLED
+    assert parsed_items[1]["status"] == CANCELLED
+    assert parsed_items[2]["status"] == PASSED
+    assert parsed_items[3]["status"] == PASSED
+    assert parsed_items[4]["status"] == PASSED
+    assert parsed_items[5]["status"] == PASSED
+
+
+def test_location():
+    # annual meeting
+    # will begin prior to the Board of Commissioners Meeting in the CMHA Boardroom located at 1635 Western Avenue (45214) beginning at 5:45pm
+    # other meeting
+    # will take place at the The Reserve on South Martin, 7363 Martin Street, Cincinnati, OH 45231 (Mt. Healthy) beginning at 6pm.
+    # will take place in the West Union Square, 2942 Banning Road, Cincinnati, OH 45239 beginning at 6pm.
+    # will take place in the West Union Square, 2942 Banning Road, Cincinnati, OH 45239 beginning at 6pm.
+    assert parsed_items[0]["location"] == {
+        "name": "CMHA Boardroom",
+        "address": "1635 Western Avenue (45214)"
+    }
+    assert parsed_items[1]["location"] == {
+        "name": "CMHA Boardroom",
+        "address": "1635 Western Avenue (45214)"
+    }
+    assert parsed_items[2]["location"] == {
+        "name": "CMHA Boardroom",
+        "address": "1635 Western Avenue (45214)"
+    }
+    assert parsed_items[3]["location"] == {
+        "name": "West Union Square",
+        "address": "2942 Banning Road, Cincinnati, OH 45239"
+    }
+    assert parsed_items[4]["location"] == {
+        "name": "The Reserve on South Martin",
+        "address": "7363 Martin Street, Cincinnati, OH 45231 (Mt. Healthy)"
+    }
+    assert parsed_items[5]["location"] == {
+        "name": "CMHA Boardroom",
+        "address": "1635 Western Avenue (45214)"
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://cintimha.com/wp-admin/admin-ajax.php?action=get_calendar_events&noheader=true&start_date=1719938796&end_date=1751475199&show_expired=true&event_category_id=executive-office-1466450825"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "title": "Event Link",
+        "href": "https://cintimha.com/events/board-of-commissioners-meeting-63/"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMISSION
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
